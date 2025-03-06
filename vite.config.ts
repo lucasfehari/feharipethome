00b26778ffc525bee3918e1316496a35d65c1813
lucasfehari <lucasfeharioficial@gmail.com>
2025-03-06 10:59:32 -0300

    Configurar deploy no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/feharipethome/", // 🔥 Substitua pelo nome do repositório do GitHub
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
